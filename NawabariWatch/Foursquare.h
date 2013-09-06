8b4c6298fa07a38f413250a135f29400168113ff
takkjm <tak.kjm@gmail.com>
2013-09-06 14:37:13 +0900

    Foursquareの認証まで

---
+++ NawabariWatch/Foursquare.h
@@ -0,0 +1,13 @@
+//
+//  Foursquare.h
+//  NawabariWatch
+//
+//  Created by techcamp on 2013/09/06.
+//  Copyright (c) 2013年 Nao Minami. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Foursquare : NSObject
+
+@end
---
+++ NawabariWatch/Foursquare.m
@@ -0,0 +1,13 @@
+//
+//  Foursquare.m
+//  NawabariWatch
+//
+//  Created by techcamp on 2013/09/06.
+//  Copyright (c) 2013年 Nao Minami. All rights reserved.
+//
+
+#import "Foursquare.h"
+
+@implementation Foursquare
+
+@end
